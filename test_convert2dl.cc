832e628e55df7063c4dde842d258fe21ae354d24
bo-yang <bonny95@gmail.com>
2014-09-02 21:34:25 -0700

    Add function Convert2DL.

---
+++ test_convert2dl.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "BinaryTree.h"
+
+using namespace std;
+
+int main() {
+	vector<string> tree={"1","2","3","#","#","4","5","6","#","7","8"};
+
+	cout<<"\nBinary Tree(left-right):"<<endl;
+	BinaryTree bt(tree);
+	TreeNode* t=bt.GetRoot();
+	bt.PrintTree(t);
+	cout<<"\nBinary Tree(down-right):"<<endl;
+	t=bt.Convert2DL(t);
+	bt.PrintInDL(t);
+
+	return 0;
+}
